6b0374722413d4812628527e7ff6bcaeb930ed7f
Juho Jeong <aspectran@gmail.com>
2020-03-21 01:35:37 +0900

    Update

--- setup/scripts/1-pull.sh
+++ setup/scripts/1-pull.sh
@@ -5,7 +5,7 @@
 if [ ! -d "$REPO_DIR" ]; then
   [ ! -d "$BUILD_DIR" ] && mkdir "$BUILD_DIR"
   cd "$BUILD_DIR" || exit
-  git clone "$REPO" "$APP_NAME"
+  git clone "$REPO_URL" "$APP_NAME"
 else
   cd "$REPO_DIR" || exit
   git pull origin master
