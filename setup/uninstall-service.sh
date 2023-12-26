835f08a437d4e38527ae886face7afc113714054
Juho Jeong <aspectran@gmail.com>
2023-12-26 17:47:13 +0900

    Update

--- setup/uninstall-service.sh
+++ setup/uninstall-service.sh
@@ -5,8 +5,8 @@
 echo "Uninstalling /etc/init.d/$APP_NAME ..."
 
 if [ -f "/etc/init.d/$APP_NAME" ]; then
-  sudo rm "/etc/init.d/$APP_NAME" || exit
   sudo update-rc.d "$APP_NAME" remove || exit
+  sudo rm "/etc/init.d/$APP_NAME" || exit
   echo "Service $APP_NAME has been uninstalled successfully."
 else
   echo "Service $APP_NAME could not be found."
